7630991536ab9516f8340419127088f52e5c6e3a
Ania <124797307+aniakurtiak@users.noreply.github.com>
2023-10-19 23:55:06 +0300

    add Route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,15 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { Layout } from './Layout/Layout.styled';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import { fetchContacts } from 'redux/operations';
 import { selectError, selectIsLoading } from 'redux/selectors';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../pages/Home'));
+const RegisterPage = lazy(() => import('../pages/Register'));
+const LoginPage = lazy(() => import('../pages/Login'));
+const ContactsPage = lazy(() => import('../pages/Contacts'));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -17,11 +23,18 @@ export const App = () => {
   }, [dispatch]);
 
   return (
-    <Layout>
-      <ContactForm />
-      <Filter />
-      {isLoading && !error && <b>Request in progress...</b>}
-      <ContactList />
-    </Layout>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+
+        {/* <ContactForm />
+        <Filter />
+        {isLoading && !error && <b>Request in progress...</b>}
+        <ContactList /> */}
+      </Route>
+    </Routes>
   );
 };
